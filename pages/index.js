b6174e97c5b5d60f43971ff6f24a7fc1d97bc2d1
Petr Palata <petrpalata@Petrs-MBP.lan>
2022-03-01 15:25:15 +0100

    FingerPrintJS visitorID retrival

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useState } from 'react'
+import React from 'react';
+import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
+
+
 
 export default function Home() {
+	const [visitorId, setVisitorId] = React.useState("")
+
+
+	function handleButtonClick() {
+		console.log("button clicked");
+		// Initialize an agent at application startup.
+		const fpPromise = FingerprintJS.load({
+			token: 'tXDGqdPGX6CGFZ8ofXr1', region: 'eu'
+		})
+
+		// Get the visitor identifier when you need it.
+		fpPromise
+			.then(fp => fp.get())
+			.then(result => setVisitorId(result.visitorId))
+	}
+
   return (
     <div className="container">
       <Head>
@@ -11,10 +32,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="FingerPrintJS PRO test" />
+	<button onClick={handleButtonClick}>Get Visitor Id</button>
+	<div>{visitorId}</div>
       </main>
 
       <Footer />
